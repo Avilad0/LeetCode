a37b287fdd981e44469e5c70dd5159a7a4de7275
Avi Lad <avilad1010@gmail.com>
2024-07-13 12:48:46 -0400

    Added file: 2751_RobotCollisions.cpp

---
+++ 2751_RobotCollisions.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> survivedRobotsHealths(vector<int>& positions, vector<int>& healths, string directions) {
+        ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+
+
+   
+
+    }
+};
+
+
+        // position, index
+    //  priority_queue<pair<int,int>> leftMoving, rightMoving;
+    //     int n = positions.size(),i;
+
+    //     for (i=0;i<n;++i){
+    //         if(directions[i]=='L'){
+    //             leftMoving.emplace(positions[i], i);
+    //         } else{
+    //             rightMoving.emplace(positions[i], i);
+    //         }
+    //     }
+
+    //     while (!(leftMoving.empty() || rightMoving.empty())){
+    //         if (rightMoving.top().first<= leftMoving.top().first)
+    //     }
+
+
+//     1   4   8    12             ->
+//   0   2    5   11    13  16        <-
