8c0a9e73b3669d023c6ea6ce038962fdfc4f4dd2
Avi Lad <avilad1010@gmail.com>
2024-07-31 13:21:36 -0400

    Added file: 1105_FillingBookcaseShelves.cpp

---
+++ 1105_FillingBookcaseShelves.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
+        
+        i
+
+        for(auto& b: books){
+
+        }
+    }
+};
