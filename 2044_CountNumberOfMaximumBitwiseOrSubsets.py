20e9996c58995aecba81f50487ae78ade1c73111
Avi Lad <avilad1010@gmail.com>
2024-10-17 22:21:36 -0400

    Added file: 2044_CountNumberOfMaximumBitwiseOrSubsets.py

---
+++ 2044_CountNumberOfMaximumBitwiseOrSubsets.py
@@ -0,0 +1,17 @@
+from typing import List
+
+class Solution:
+    def countMaxOrSubsets(self, nums: List[int]) -> int:
+        a
+
+
+
+
+# Input: nums = [3,2,1,5]
+# Output: 6
+
+# 1 = [1,3,5]
+# 10 = [2,3]
+# 100 = [5]
+
+# 
