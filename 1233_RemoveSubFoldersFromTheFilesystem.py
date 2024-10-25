407145cb061f1a8435587dc3a7690389871841c4
Avi Lad <avilad1010@gmail.com>
2024-10-25 18:30:44 -0400

    Added file: 1233_RemoveSubFoldersFromTheFilesystem.py

---
+++ 1233_RemoveSubFoldersFromTheFilesystem.py
@@ -0,0 +1,12 @@
+from typing import List
+
+class Solution:
+    def removeSubfolders(self, folder: List[str]) -> List[str]:
+        folder.sort()
+
+        result = [folder[0]]
+        for i in range(1,len(folder)):
+            if not folder[i].startswith(result[-1] + "/"):
+                result.append(folder[i])
+
+        return result
