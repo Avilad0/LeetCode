66f6e235204e270ba78efffb94beb01af3fbccb2
Avi Lad <avilad1010@gmail.com>
2025-07-01 10:40:13 -0400

    Updated file: 1395_CountNumberOfTeams.cpp

--- 1395_CountNumberOfTeams.cpp
+++ 1395_CountNumberOfTeams.cpp
@@ -1,17 +1,36 @@
 #include<bits/stdc++.h>
 using namespace std;
 
+// tc = O(n**2)
 class Solution {
-public:
+    public:
     int numTeams(vector<int>& rating) {
         ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+        int n = rating.size(), ans = 0;
+        
+        for (int i = 1; i<n-1; ++i){
+            int lessThanOnLeft = 0 , lessThanOnRight = 0, greaterThanOnLeft = 0, greaterThanOnRight = 0;
+            for (int j=0; j<i; ++j){
+                if (rating[i]<rating[j]) ++greaterThanOnLeft;
+                else if (rating[i]>rating[j]) ++lessThanOnLeft;
+            }
+            
+            for (int j=i+1; j<n; ++j){
+                if (rating[i]<rating[j]) ++greaterThanOnRight;
+                else if (rating[i]>rating[j]) ++lessThanOnRight;
+            }
+
+            ans += lessThanOnLeft*greaterThanOnRight + greaterThanOnLeft*lessThanOnRight;
+        }
+
+        return ans;
     }
 };
 
 
 
 
-// // Very Slow
+// // Very Slow, tc = O(n**3)
 // class Solution {
 // public:
 //     int numTeams(vector<int>& rating) {
