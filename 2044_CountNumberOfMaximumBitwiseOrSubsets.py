9f1fd8aad8bfc45641e680abdce7c89079863df0
Avi Lad <avilad1010@gmail.com>
2025-07-20 20:27:55 -0400

    Updated file: 2044_CountNumberOfMaximumBitwiseOrSubsets.py

--- 2044_CountNumberOfMaximumBitwiseOrSubsets.py
+++ 2044_CountNumberOfMaximumBitwiseOrSubsets.py
@@ -2,9 +2,26 @@ from typing import List
 
 class Solution:
     def countMaxOrSubsets(self, nums: List[int]) -> int:
-        a
+        n = len(nums)
+        
+        maxOr = 0
+        for num in nums:
+            maxOr |= num
 
+        memo = {}
+        
+        def dfs(index, currOr):
+            if index==n:
+                return int(currOr==maxOr)
+            
+            key = (index,currOr)
+            if key in memo:
+                return memo[key]
 
+            memo[key] = dfs(index+1, currOr) + dfs(index+1, currOr|nums[index])
+            return memo[key]
+
+        return dfs(0,0)                
 
 
 # Input: nums = [3,2,1,5]
