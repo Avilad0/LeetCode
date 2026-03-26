a8cf6a4e489e05df4f40a866e94c8f7981f7e784
Avi Lad <avilad1010@gmail.com>
2026-03-26 18:50:39 -0400

    Updated file: 242_ValidAnagram.py

--- 242_ValidAnagram.py
+++ 242_ValidAnagram.py
@@ -1,10 +1,28 @@
 class Solution:
-    # def isAnagram(self, s: str, t: str) -> bool:
-    #     if len(s) != len(t):
-    #         return False
-    #     return ''.join(sorted(list(s))) == ''.join(sorted(list(t)))
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        if len(s)!=len(t):
+            return False
+        
+        n=len(s)
+        charFreq = [0]*26
+        for i in range(n):
+            charFreq[ord(s[i])-97] +=1
+            charFreq[ord(t[i])-97] -=1
+
+        for f in charFreq:
+            if f!=0:
+                return False
+        return True
+
+
+# class Solution:
+#     # def isAnagram(self, s: str, t: str) -> bool:
+#     #     if len(s) != len(t):
+#     #         return False
+#     #     return ''.join(sorted(list(s))) == ''.join(sorted(list(t)))
         
 
-    def isAnagram(self, s: str, t: str) -> bool:
-        return sorted(s) == sorted(t)
+#     def isAnagram(self, s: str, t: str) -> bool:
+#         return sorted(s) == sorted(t)
         
