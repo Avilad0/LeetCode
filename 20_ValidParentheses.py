c4e681efecbf6ba3663859bb4ae09cfd8162b288
Avi Lad <avilad1010@gmail.com>
2026-03-28 13:52:54 -0400

    Updated file: 20_ValidParentheses.py

--- 20_ValidParentheses.py
+++ 20_ValidParentheses.py
@@ -1,48 +1,15 @@
 class Solution:
     def isValid(self, s: str) -> bool:
-        
-        dic = {"[":"]", "{":"}", "(":")"}
-        dic_set = dic.keys()
-        stack = []
-        i=-1
+        openToCloseMap = {'(':')', '{':'}', '[':']'}
+
+        stck = []
         for c in s:
-            if c in dic_set:
-                stack.append(c)
-                i+=1
-            elif i==-1 or c!= dic[stack.pop()]:
-                return False
+            if c in openToCloseMap:
+                stck.append(c)
             else:
-                i-=1
-
-        return i==-1
-    
-
-        # stack = []
-        # i=-1
-        # for c in s:
-        #     if c == "]":
-        #         if i==-1 or stack[i]!="[":
-        #             return False
-        #         else:
-        #             stack.pop()
-        #             i-=1
-
-        #     elif c == "}":
-        #         if i==-1 or stack[i]!="{":
-        #             return False
-        #         else:
-        #             stack.pop()
-        #             i-=1
-
-        #     elif c == ")":
-        #         if i==-1 or stack[i]!="(":
-        #             return False
-        #         else:
-        #             stack.pop()
-        #             i-=1
-        #     else:
-        #         stack.append(c)
-        #         i+=1
-
-        # return i==-1
-
+                if stck and openToCloseMap[stck[-1]] == c:
+                    stck.pop()
+                else:
+                    return False
+        
+        return not stck   
