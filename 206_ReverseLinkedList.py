7f7c4f491350b7ef639a1006cd1717e9f174c22b
Avi Lad <avilad1010@gmail.com>
2026-03-28 20:41:45 -0400

    Updated file: 206_ReverseLinkedList.py

--- 206_ReverseLinkedList.py
+++ 206_ReverseLinkedList.py
@@ -5,18 +5,18 @@ class ListNode:
         self.val = val
         self.next = next
 
+# Iterative Soltion. tc= O(n), sc=O(1)    
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if head==None or head.next==None:
-            return head
-        prev = None
-        curr = head
-        while curr!=None:
-            nxt = curr.next
-            curr.next=prev
-            prev = curr
-            curr = nxt
-        return prev
+        node = head
+        newHead = None
+        while node:
+            temp = node.next
+            node.next = newHead
+            newHead = node
+            node = temp
+        
+        return newHead
 
 
 l =ListNode(1,ListNode(2,None))
@@ -27,3 +27,26 @@ l = Solution().reverseList(l)
 while l!=None:
     print(l.val)
     l = l.next
+
+
+# # Alternative recursive Soltion. tc= O(n), sc=O(n)
+# class Solution:
+
+#     def recur(self, node: Optional[ListNode]) -> Optional[ListNode]:
+#         if node==None:
+#             return None
+        
+#         newParent = self.recur(node.next)
+#         if newParent==None:
+#             self.newHead = node
+#         else:
+#             newParent.next=node
+#         return node
+
+#     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+#         if not head:
+#             return None
+            
+#         self.recur(head)
+#         head.next=None
+#         return self.newHead
