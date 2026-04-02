8b31a2864eb1da4bf94b44806429e0fd8138ba79
Avi Lad <avilad1010@gmail.com>
2026-04-01 21:02:37 -0400

    Added file: 198_HouseRobber.py

--- 198_HouseRobber.py
+++ 198_HouseRobber.py
@@ -1,11 +1,21 @@
 from typing import List
 
+# same as prev but more clear
 class Solution:
-    def rob(self, inputArr: List[int]) -> int:
-            n=len(inputArr)
-            dp = [0]*(3)
+    def rob(self, nums: List[int]) -> int:
+        prevPrevHouse, prevHouse = 0, 0
+
+        for num in nums:
+            prevPrevHouse, prevHouse=prevHouse, max(prevHouse, num+prevPrevHouse)
+
+        return prevHouse
+
+# class Solution:
+#     def rob(self, inputArr: List[int]) -> int:
+#             n=len(inputArr)
+#             dp = [0]*(3)
             
-            for i in range(n):
-                dp[(i+1)%3] = max(dp[(i-1)%3] + inputArr[i], dp[i%3])
+#             for i in range(n):
+#                 dp[(i+1)%3] = max(dp[(i-1)%3] + inputArr[i], dp[i%3])
                 
-            return dp[n%3]
+#             return dp[n%3]
