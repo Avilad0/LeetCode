e5da9beb750611f40302ea0b0604e500aaea9375
Avi Lad <avilad1010@gmail.com>
2026-04-02 14:42:00 -0400

    Added file: 139_WordBreak.py

--- 139_WordBreak.py
+++ 139_WordBreak.py
@@ -2,19 +2,44 @@ from typing import List
 
 class Solution:
     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
-        
-        l = len(s)+1
-        dp = [False]*l
-        dp[0] = True
-        wordDictSet = set(wordDict)
-        
-        for j in range(1, l):
-            for i in range(0,j):
-                if dp[i] and s[i:j] in wordDictSet:
-                    dp[j] = True
-                    break
+        wordSet = set()
+        maxLen = 0
+        for word in wordDict:
+            maxLen = max(maxLen,len(word))
+            wordSet.add(word)
 
-        return dp[l-1]
+        n = len(s)
+        memo = {n:True}
+        def backtrack(start):
+            if start in memo:
+                return memo[start]
+            
+            for end in range(start+1, min(start+1+maxLen, n+1)):
+                if s[start:end] in wordSet and backtrack(end):
+                    memo[start] = True
+                    return True
+            
+            memo[start] = False
+            return False
+
+        return backtrack(0)
+
+
+# class Solution:
+#     def wordBreak(self, s: str, wordDict: List[str]) -> bool:
+        
+#         l = len(s)+1
+#         dp = [False]*l
+#         dp[0] = True
+#         wordDictSet = set(wordDict)
+        
+#         for j in range(1, l):
+#             for i in range(0,j):
+#                 if dp[i] and s[i:j] in wordDictSet:
+#                     dp[j] = True
+#                     break
+
+#         return dp[l-1]
 
 
         # dp = [[-1 for _ in __] for __ in wordDict]
