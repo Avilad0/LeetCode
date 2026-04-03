6b88bfcba02095acdcbb76c7400783387fb968c4
Avi Lad <avilad1010@gmail.com>
2026-04-03 01:33:42 -0400

    Updated file: 48_RotateImage.py

--- 48_RotateImage.py
+++ 48_RotateImage.py
@@ -5,11 +5,46 @@ class Solution:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        n= len(matrix)
-        for i in range(n):
-            for j in range(i):
-                matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
+        n = len(matrix)
+        left, right = 0, n-1        
+
+        while left<right:
+            for i in range(right-left):
+                top, bottom = left, right
+
+                temp = matrix[top][left+i]
+                matrix[top][left+i] = matrix[bottom-i][left]
+                matrix[bottom-i][left] = matrix[bottom][right-i]
+                matrix[bottom][right-i] = matrix[top+i][right]
+                matrix[top+i][right] = temp
+
+
+# class Solution:
+#     def rotate(self, matrix: List[List[int]]) -> None:
+#         """
+#         Do not return anything, modify matrix in-place instead.
+#         """
+
+#         n = len(matrix)
+#         for i in range(n//2):
+#             for j in range(i,n-i-1):
+#                 curr = matrix[i][j]
+#                 matrix[i][j] = matrix[n-1-j][i]
+#                 matrix[n-1-j][i] = matrix[n-1-i][n-1-j]
+#                 matrix[n-1-i][n-1-j] = matrix[j][n-1-i]
+#                 matrix[j][n-1-i] = curr
+
+
+# class Solution:
+#     def rotate(self, matrix: List[List[int]]) -> None:
+#         """
+#         Do not return anything, modify matrix in-place instead.
+#         """
+#         n= len(matrix)
+#         for i in range(n):
+#             for j in range(i):
+#                 matrix[i][j], matrix[j][i] = matrix[j][i], matrix[i][j]
         
-        for i in range(n):
-            for j in range(n//2):
-                matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
+#         for i in range(n):
+#             for j in range(n//2):
+#                 matrix[i][j], matrix[i][n-1-j] = matrix[i][n-1-j], matrix[i][j]
