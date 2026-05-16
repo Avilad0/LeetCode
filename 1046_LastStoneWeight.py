9a8c81b87eaa832b5e614ec37d2baa67eb9c747a
Avi Lad <avilad1010@gmail.com>
2026-05-16 10:39:57 -0400

    Updated file: 1046_LastStoneWeight.py

--- 1046_LastStoneWeight.py
+++ 1046_LastStoneWeight.py
@@ -1,6 +1,7 @@
 from typing import List
 import heapq
 
+# tc= O(nlogn), sc=O(n) - using maxHeap
 class Solution:
     def lastStoneWeight(self, stones: List[int]) -> int:
         heap = [-x for x in stones]
@@ -13,3 +14,36 @@ class Solution:
                 heapq.heappush(heap, -newS)
         
         return 0 if len(heap)==0 else -heap[0]
+    
+
+# tc= O(n + max(stones)), sc=O(max(stones)) - using bucket sort
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        maxS = max(stones)
+        freq = [0]*(maxS+1)
+
+        for stone in stones:
+            freq[stone]+=1
+        
+        i = maxS
+        while i>=0:
+            x = i
+            freq[i]-=1
+            while i>=0 and freq[i]==0:
+                i-=1
+            
+            if i<0:
+                return x
+
+            y=i
+            freq[i]-=1
+
+            newS = abs(x-y)
+            if newS>0:
+                freq[newS]+=1
+
+            i=x
+            while i>=0 and freq[i]==0:
+                i-=1
+
+        return 0
