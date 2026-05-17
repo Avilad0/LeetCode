3f9c81976f562f1ba01799819dec060b56ad1616
Avi Lad <avilad1010@gmail.com>
2026-05-17 10:24:06 -0400

    Updated file: 7_ReverseInteger.py

--- 7_ReverseInteger.py
+++ 7_ReverseInteger.py
@@ -1,18 +1,30 @@
+import math
+
+#tc=O(1), sc=O(1)   #32 bit ints
 class Solution:
     def reverse(self, x: int) -> int:
-        mul = -1 if x<0 else 1
-        ans = 0
+        maxInt = 0x7FFFFFFF
+        maxIntDiv10 = maxInt//10
+        maxIntMod10 = maxInt%10
 
-        x*=mul
-        while x!=0:
-            r = x%10
-            ans = ans*10 + r
-            x=x//10
+        minInt = -maxInt-1
+        minIntDiv10 = int(minInt/10)
+        minIntMod10 = int(math.fmod(minInt,10))
 
-        m = 2**31
-        if (mul==-1 and ans>m) or (mul==1 and ans>=m):
-            return 0
+        y=0
+        while x:
+            # use int of math.fmod instead of % for c++/java like mod calculations of negative numbers. 
+            # eg. -18fmod10 =-8.0 vs -18%10=2
+            lastDigit = int(math.fmod(x,10))
+            # use int of / and not //.
+            # eg: int(-18/10) = -1 vs -18//10=-2
+            x=int(x/10)
 
-        return ans*mul
-    
-print(Solution().reverse(-123))
+            if y>maxIntDiv10 or (y==maxIntDiv10 and lastDigit>maxIntMod10):
+                return 0
+            if y<minIntDiv10 or (y==minIntDiv10 and lastDigit<minIntMod10):
+                return 0
+            
+            y=y*10+lastDigit
+        
+        return y
