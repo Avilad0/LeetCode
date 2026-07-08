532c73867662102ffe656e42bfa7b61a40d7e1da
Avi Lad <avilad1010@gmail.com>
2026-07-08 18:41:43 -0400

    Updated file: 790_DominoAndTrominoTiling.py

--- 790_DominoAndTrominoTiling.py
+++ 790_DominoAndTrominoTiling.py
@@ -4,12 +4,12 @@ class Solution:
         MOD = 10**9 + 7
         dp = [0]*(n+1)
 
-        dp[0]=1
+        dp[0]=dp[1]=1
 
-        for i in range(1,n+1):
+        for i in range(2,n+1):
             dp[i]= (2*dp[i-1] + (0 if i<3 else dp[i-3]) )%MOD
 
-        return dp[0][0]
+        return dp[n]
 
 '''
 dp[n] = dp[n-1] (|) +dp[n-2] (=) + 2*(dp[n-3]+dp[n-4]....+dp[0]) (|-____|)(|____-|)
